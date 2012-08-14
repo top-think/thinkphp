1aada81b33ff3fff708723b34c441548dd9bdf14
thinkphp <thinkphp@qq.com>
2012-08-14 17:30:03 +0800

    修改COOKIE_EXPIRE的默认值为0 和系统保持一致

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -37,7 +37,7 @@ return  array(
     'ACTION_SUFFIX'         =>  '', // 操作方法后缀
 
     /* Cookie设置 */
-    'COOKIE_EXPIRE'         => 3600,    // Coodie有效期
+    'COOKIE_EXPIRE'         => 0,    // Coodie有效期
     'COOKIE_DOMAIN'         => '',      // Cookie有效域名
     'COOKIE_PATH'           => '/',     // Cookie路径
     'COOKIE_PREFIX'         => '',      // Cookie前缀 避免冲突
