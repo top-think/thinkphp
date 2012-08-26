f8f75a3b7bc47e120fba4ce159a55f04d556ff6d
thinkphp <thinkphp@qq.com>
2012-08-26 20:01:32 +0800

    G方法改进，支持内存使用记录和统计，记录方法不变，需要统计内存使用情况 ，使用 G('begin','end','m'); 表示统计begin到end区间的内存使用情况

--- ThinkPHP/Common/common.php
+++ ThinkPHP/Common/common.php
@@ -17,26 +17,51 @@
  */
 
 /**
- * 记录和统计时间（微秒）
+ * 记录和统计时间（微秒）和内存使用情况
+ * 使用方法:
+ * <code>
+ * G('begin'); // 记录开始标记位
+ * // ... 区间运行代码
+ * G('end'); // 记录结束标签位
+ * echo G('begin','end',6); // 统计区间运行时间 精确到小数后6位
+ * echo G('begin','end','m'); // 统计区间内存使用情况
+ * 如果end标记位没有定义，则会自动以当前作为标记位
+ * 其中统计内存使用需要 MEMORY_LIMIT_ON 常量为true才有效
+ * </code>
  * @param string $start 开始标签
  * @param string $end 结束标签
- * @param integer $dec 小数位 
+ * @param integer|string $dec 小数位或者m 
  * @return mixed
  */
 function G($start,$end='',$dec=4) {
-    static $_info = array();
+    static $_info   =   array();
+    static $_mem    =   array();
     if(is_float($end)) { // 记录时间
         $_info[$start]  =  $end;
-    }elseif(!empty($end)){ // 统计时间
+    }elseif(!empty($end)){ // 统计时间和内存使用
         if(!isset($_info[$end])) $_info[$end]   =  microtime(TRUE);
-        return number_format(($_info[$end]-$_info[$start]),$dec);
-    }else{ // 记录时间
+        if(MEMORY_LIMIT_ON && $dec=='m'){
+            if(!isset($_mem[$end])) $_mem[$end]   =  memory_get_usage();
+            return number_format(($_mem[$end]-$_mem[$start])/1024);          
+        }else{
+            return number_format(($_info[$end]-$_info[$start]),$dec);
+        }       
+            
+    }else{ // 记录时间和内存使用
         $_info[$start]  =  microtime(TRUE);
+        if(MEMORY_LIMIT_ON) $_mem[$start]  =  memory_get_usage();
     }
 }
 
 /**
  * 设置和获取统计数据
+ * 使用方法:
+ * <code>
+ * N('db',1); // 记录数据库操作次数
+ * N('read',1); // 记录读取次数
+ * echo N('db'); // 获取当前页面数据库的所有操作次数
+ * echo N('read'); // 获取当前页面读取次数
+ * </code> 
  * @param string $key 标识位置
  * @param integer $step 步进值
  * @return mixed
