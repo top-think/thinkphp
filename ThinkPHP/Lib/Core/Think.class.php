42f9f8dea084940a0b98bddb6346ebe28d8b84f5
luofei614 <upfy@qq.com>
2012-09-02 10:08:52 +0800

    修复一个页面压缩的bug

--- ThinkPHP/Lib/Core/Think.class.php
+++ ThinkPHP/Lib/Core/Think.class.php
@@ -236,6 +236,7 @@ class Think {
           case E_COMPILE_ERROR:
           case E_USER_ERROR:
             ob_end_clean();
+            if(!ini_get('zlib.output_compression') && C('OUTPUT_ENCODE')) ob_start('ob_gzhandler');
             $errorStr = "$errstr ".$errfile." 第 $errline 行.";
             if(C('LOG_RECORD')) Log::write("[$errno] ".$errorStr,Log::ERR);
             function_exists('halt')?halt($errorStr):exit('ERROR:'.$errorStr);
