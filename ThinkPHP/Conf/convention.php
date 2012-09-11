e6f63602805cbe4440e283e80090d1d48a138b99
thinkphp <thinkphp@qq.com>
2012-09-11 22:53:03 +0800

    增加DATA_CACHE_PREFIX参数 用于统一设置缓存前缀

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -76,6 +76,7 @@ return  array(
     'DATA_CACHE_TIME'       => 0,      // 数据缓存有效期 0表示永久缓存
     'DATA_CACHE_COMPRESS'   => false,   // 数据缓存是否压缩缓存
     'DATA_CACHE_CHECK'      => false,   // 数据缓存是否校验缓存
+    'DATA_CACHE_PREFIX'     => '',     // 缓存前缀
     'DATA_CACHE_TYPE'       => 'File',  // 数据缓存类型,支持:File|Db|Apc|Memcache|Shmop|Sqlite|Xcache|Apachenote|Eaccelerator
     'DATA_CACHE_PATH'       => TEMP_PATH,// 缓存路径设置 (仅对File方式缓存有效)
     'DATA_CACHE_SUBDIR'     => false,    // 使用子目录缓存 (自动根据缓存标识的哈希创建子目录)
