9d253fd0feec818efbe803bd1a627778bcfb6feb
thinkphp <thinkphp@qq.com>
2012-09-15 10:20:51 +0800

    改进UploadFile类

--- Extend/Library/ORG/Net/UploadFile.class.php
+++ Extend/Library/ORG/Net/UploadFile.class.php
@@ -19,29 +19,29 @@
 class UploadFile {//类定义开始
 
     private $config =   array(
-        'maxSize' => -1,    // 上传文件的最大值
-        'supportMulti' => true,    // 是否支持多文件上传
-        'allowExts'=> array(),    // 允许上传的文件后缀 留空不作后缀检查
-        'allowTypes' => array(),    // 允许上传的文件类型 留空不做检查
-        'thumb'   =>  false,    // 使用对上传图片进行缩略图处理
-        'imageClassPath' => 'ORG.Util.Image',    // 图库类包路径
-        'thumbMaxWidth'   =>'',// 缩略图最大宽度
-        'thumbMaxHeight'    =>'',// 缩略图最大高度
-        'thumbPrefix'   =>  'thumb_',// 缩略图前缀
-        'thumbSuffix'  =>  '',
-        'thumbPath' => '',// 缩略图保存路径
-        'thumbFile'		=>'',// 缩略图文件名
-        'thumbRemoveOrigin' => false,// 是否移除原图
-        'zipImages' => false,// 压缩图片文件上传
-        'autoSub'   =>  false,// 启用子目录保存文件
-        'subType'   => 'hash',// 子目录创建方式 可以使用hash date
-        'dateFormat' => 'Ymd',
-        'hashLevel' =>  1, // hash的目录层次
-        'savePath' => '',// 上传文件保存路径
-        'autoCheck' => true, // 是否自动检查附件
-        'uploadReplace' => false,// 存在同名是否覆盖
-        'saveRule'=>'',// 上传文件命名规则
-        'hashType'=>'md5_file',// 上传文件Hash规则函数名
+        'maxSize'           =>  -1,    // 上传文件的最大值
+        'supportMulti'      =>  true,    // 是否支持多文件上传
+        'allowExts'         =>  array(),    // 允许上传的文件后缀 留空不作后缀检查
+        'allowTypes'        =>  array(),    // 允许上传的文件类型 留空不做检查
+        'thumb'             =>  false,    // 使用对上传图片进行缩略图处理
+        'imageClassPath'    =>  'ORG.Util.Image',    // 图库类包路径
+        'thumbMaxWidth'     =>  '',// 缩略图最大宽度
+        'thumbMaxHeight'    =>  '',// 缩略图最大高度
+        'thumbPrefix'       =>  'thumb_',// 缩略图前缀
+        'thumbSuffix'       =>  '',
+        'thumbPath'         =>  '',// 缩略图保存路径
+        'thumbFile'         =>  '',// 缩略图文件名
+        'thumbRemoveOrigin' =>  false,// 是否移除原图
+        'zipImages'         =>  false,// 压缩图片文件上传
+        'autoSub'           =>  false,// 启用子目录保存文件
+        'subType'           =>  'hash',// 子目录创建方式 可以使用hash date
+        'dateFormat'        =>  'Ymd',
+        'hashLevel'         =>  1, // hash的目录层次
+        'savePath'          =>  '',// 上传文件保存路径
+        'autoCheck'         =>  true, // 是否自动检查附件
+        'uploadReplace'     =>  false,// 存在同名是否覆盖
+        'saveRule'          =>  'uniqid',// 上传文件命名规则
+        'hashType'          =>  'md5_file',// 上传文件Hash规则函数名
         );
 
     // 错误信息
