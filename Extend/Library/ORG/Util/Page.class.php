8a0f1412eeacb042127205fd775b1a3458fc3d02
thinkphp <thinkphp@qq.com>
2012-09-17 22:42:55 +0800

    修正Page类对URL_CASE_INSENSITIVE和parameter参数的处理

--- Extend/Library/ORG/Util/Page.class.php
+++ Extend/Library/ORG/Util/Page.class.php
@@ -9,7 +9,7 @@
 // | Author: liu21st <liu21st@gmail.com>
 // |         lanfengye <zibin_5257@163.com>
 // +----------------------------------------------------------------------
-// $Id: Page.class.php 2998 2012-06-13 14:12:18Z liu21st@gmail.com $
+
 class Page {
 	
     // 分页栏每页显示的页数
@@ -34,30 +34,26 @@ class Page {
     protected $varPage;
 
     /**
-     +----------------------------------------------------------
      * 架构函数
-     +----------------------------------------------------------
      * @access public
-     +----------------------------------------------------------
      * @param array $totalRows  总的记录数
      * @param array $listRows  每页显示记录数
      * @param array $parameter  分页跳转的参数
-     +----------------------------------------------------------
      */
     public function __construct($totalRows,$listRows='',$parameter='') {
-        $this->totalRows = $totalRows;
-        $this->parameter = $parameter;
-        $this->varPage = C('VAR_PAGE') ? C('VAR_PAGE') : 'p' ;
+        $this->totalRows    =   $totalRows;
+        $this->parameter    =   $parameter;
+        $this->varPage      =   C('VAR_PAGE') ? C('VAR_PAGE') : 'p' ;
         if(!empty($listRows)) {
-            $this->listRows = intval($listRows);
+            $this->listRows =   intval($listRows);
         }
-        $this->totalPages = ceil($this->totalRows/$this->listRows);     //总页数
-        $this->coolPages  = ceil($this->totalPages/$this->rollPage);
-        $this->nowPage  = !empty($_GET[$this->varPage])?intval($_GET[$this->varPage]):1;
+        $this->totalPages   =   ceil($this->totalRows/$this->listRows);     //总页数
+        $this->coolPages    =   ceil($this->totalPages/$this->rollPage);
+        $this->nowPage      =   !empty($_GET[$this->varPage])?intval($_GET[$this->varPage]):1;
         if(!empty($this->totalPages) && $this->nowPage>$this->totalPages) {
-            $this->nowPage = $this->totalPages;
+            $this->nowPage  =   $this->totalPages;
         }
-        $this->firstRow = $this->listRows*($this->nowPage-1);
+        $this->firstRow     =   $this->listRows*($this->nowPage-1);
     }
 
     public function setConfig($name,$value) {
@@ -67,65 +63,62 @@ class Page {
     }
 
     /**
-     +----------------------------------------------------------
      * 分页显示输出
-     +----------------------------------------------------------
      * @access public
      * @author lanfengye <zibin_5257@163.com>
-     +----------------------------------------------------------
      */
     public function show() {
         if(0 == $this->totalRows) return '';
-        $p = $this->varPage;
-        $nowCoolPage      = ceil($this->nowPage/$this->rollPage);
+        $p              =   $this->varPage;
+        $nowCoolPage    =   ceil($this->nowPage/$this->rollPage);
         
         //获取控制器名和方法名，并判断是否url不区分大小写
-        $url_case=C('URL_CASE_INSENSITIVE');
-        $module_name=  $url_case?strtolower(parse_name(MODULE_NAME)):parse_name(MODULE_NAME);
-        $action_name= $url_case?strtolower(parse_name(ACTION_NAME)):parse_name(ACTION_NAME);
+        $url_case       =   C('URL_CASE_INSENSITIVE');
+        $module_name    =   $url_case?  parse_name(MODULE_NAME) :   MODULE_NAME;
+        $action_name    =   $url_case?  parse_name(ACTION_NAME) :   ACTION_NAME;
         
         //替换附加参数中的分隔符
-        $parameter=  str_replace(array('&','='), C('URL_PATHINFO_DEPR'), $this->parameter);
+        $parameter      =   str_replace(array('&','='), C('URL_PATHINFO_DEPR'), $this->parameter);
 
         //增加附加参数
-        $url  =  __APP__.'/'.$module_name.C('URL_PATHINFO_DEPR').$action_name.$parameter;
+        $url            =   __APP__.'/'.$module_name.C('URL_PATHINFO_DEPR').$action_name.C('URL_PATHINFO_DEPR').$parameter;
         
         //上下翻页字符串
-        $upRow   = $this->nowPage-1;
-        $downRow = $this->nowPage+1;
+        $upRow          =   $this->nowPage-1;
+        $downRow        =   $this->nowPage+1;
         if ($upRow>0){
-            $upPage="<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."$upRow".C('URL_HTML_SUFFIX')."'>".$this->config['prev']."</a>";
+            $upPage     =   "<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."$upRow".C('URL_HTML_SUFFIX')."'>".$this->config['prev']."</a>";
         }else{
-            $upPage="";
+            $upPage     =   '';
         }
 
         if ($downRow <= $this->totalPages){
-            $downPage="<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."$downRow".C('URL_HTML_SUFFIX')."'>".$this->config['next']."</a>";
+            $downPage   =   "<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."$downRow".C('URL_HTML_SUFFIX')."'>".$this->config['next']."</a>";
         }else{
-            $downPage="";
+            $downPage   =   '';
         }
         // << < > >>
         if($nowCoolPage == 1){
-            $theFirst = "";
-            $prePage = "";
+            $theFirst   =   '';
+            $prePage    =   '';
         }else{
-            $preRow =  $this->nowPage-$this->rollPage;
-            $prePage = "<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."$preRow".C('URL_HTML_SUFFIX')."' >上".$this->rollPage."页</a>";
-            $theFirst = "<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."1".C('URL_HTML_SUFFIX')."' >".$this->config['first']."</a>";
+            $preRow     =   $this->nowPage-$this->rollPage;
+            $prePage    =   "<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."$preRow".C('URL_HTML_SUFFIX')."' >上".$this->rollPage."页</a>";
+            $theFirst   =   "<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."1".C('URL_HTML_SUFFIX')."' >".$this->config['first']."</a>";
         }
         if($nowCoolPage == $this->coolPages){
-            $nextPage = "";
-            $theEnd="";
+            $nextPage   =   '';
+            $theEnd     =   '';
         }else{
-            $nextRow = $this->nowPage+$this->rollPage;
-            $theEndRow = $this->totalPages;
-            $nextPage = "<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."$nextRow".C('URL_HTML_SUFFIX')."' >下".$this->rollPage."页</a>";
-            $theEnd = "<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."$theEndRow".C('URL_HTML_SUFFIX')."' >".$this->config['last']."</a>";
+            $nextRow    =   $this->nowPage+$this->rollPage;
+            $theEndRow  =   $this->totalPages;
+            $nextPage   =   "<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."$nextRow".C('URL_HTML_SUFFIX')."' >下".$this->rollPage."页</a>";
+            $theEnd     =   "<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."$theEndRow".C('URL_HTML_SUFFIX')."' >".$this->config['last']."</a>";
         }
         // 1 2 3 4 5
         $linkPage = "";
         for($i=1;$i<=$this->rollPage;$i++){
-            $page=($nowCoolPage-1)*$this->rollPage+$i;
+            $page       =   ($nowCoolPage-1)*$this->rollPage+$i;
             if($page!=$this->nowPage){
                 if($page<=$this->totalPages){
                     $linkPage .= "&nbsp;<a href='".$url.C('URL_PATHINFO_DEPR').$p.C('URL_PATHINFO_DEPR')."$page".C('URL_HTML_SUFFIX')."'>&nbsp;".$page."&nbsp;</a>";
