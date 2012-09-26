f9dd7f8c48f0a56517fcbd15901de2483f0c8220
沙包妖梦 <gongteng524702837@gmail.com>
2012-09-26 10:35:33 +0800

    可指定的JSONP回调函数
    默认jquery的“callback”

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -130,6 +130,7 @@ return  array(
     'VAR_MODULE'            => 'm',		// 默认模块获取变量
     'VAR_ACTION'            => 'a',		// 默认操作获取变量
     'VAR_AJAX_SUBMIT'       => 'ajax',  // 默认的AJAX提交变量
+	'VAR_JSONP_HANDLER'  => 'callback',
     'VAR_PATHINFO'          => 's',	// PATHINFO 兼容模式获取变量例如 ?s=/module/action/id/1 后面的参数取决于URL_PATHINFO_DEPR
     'VAR_URL_PARAMS'        => '_URL_', // PATHINFO URL参数变量
     'VAR_TEMPLATE'          => 't',		// 默认模板切换变量
