0b1594c6ea6df9850643d4b0b82eacbc789bb06a
Aoiujz <zuojiazi@vip.qq.com>
2012-09-26 22:05:58 +0800

    修改JSONP获取参数为 $_GET

--- ThinkPHP/Lib/Core/Action.class.php
+++ ThinkPHP/Lib/Core/Action.class.php
@@ -329,7 +329,7 @@ abstract class Action {
             case 'JSONP':
                 // 返回JSON数据格式到客户端 包含状态信息
                 header('Content-Type:application/json; charset=utf-8');
-                $handler  =   isset($_REQUEST[C('VAR_JSONP_HANDLER')]) ? $_REQUEST[C('VAR_JSONP_HANDLER')] : C('DEFAULT_JSONP_HANDLER');
+                $handler  =   isset($_GET[C('VAR_JSONP_HANDLER')]) ? $_GET[C('VAR_JSONP_HANDLER')] : C('DEFAULT_JSONP_HANDLER');
                 exit($handler.'('.json_encode($data).');');  
             case 'EVAL' :
                 // 返回可执行的js脚本
