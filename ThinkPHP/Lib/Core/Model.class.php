be846eb2be9a387b4438fe00c0ec81d489745947
thinkphp <thinkphp@qq.com>
2012-10-19 14:10:39 +0800

    自动验证增加notin notbetween和notequal规则

--- ThinkPHP/Lib/Core/Model.class.php
+++ ThinkPHP/Lib/Core/Model.class.php
@@ -1023,50 +1023,49 @@ class Model {
      * @param string $value 验证数据
      * @param mixed $rule 验证表达式
      * @param string $type 验证方式 默认为正则验证
-     * @param boolean $reverse 是否反向验证
      * @return boolean
      */
-    public function check($value,$rule,$type='regex', $reverse = false){
-        switch(strtolower(trim($type))) {
+    public function check($value,$rule,$type='regex'){
+        $type   =   strtolower(trim($type));
+        switch($type) {
             case 'in': // 验证是否在某个指定范围之内 逗号分隔字符串或者数组
-                $range = is_array($rule) ? $rule : explode(',',$rule);
-                $check = in_array($value ,$range);
-                break;
+            case 'notin':
+                $range   = is_array($rule)? $rule : explode(',',$rule);
+                return $type == 'in' ? in_array($value ,$range) : !in_array($value ,$range);
             case 'between': // 验证是否在某个范围
-                $range = is_array($rule) ? $rule : explode(',',$rule);
-                list($min,$max) = explode(',',$range);
-                $check = ($value>=$min && $value<=$max);
-                break;
+            case 'notbetween': // 验证是否不在某个范围            
+                if (is_array($rule)){
+                    $min    =    $rule[0];
+                    $max    =    $rule[1];
+                }else{
+                    list($min,$max)   =  explode(',',$rule);
+                }
+                return $type == 'between' ? $value>=$min && $value<=$max : $value<$min || $value>$max;
             case 'equal': // 验证是否等于某个值
-                $check = ($value == $rule);
-                break;
+            case 'notequal': // 验证是否等于某个值            
+                return $type == 'equal' ? $value == $rule : $value != $rule;
             case 'length': // 验证长度
                 $length  =  mb_strlen($value,'utf-8'); // 当前数据长度
                 if(strpos($rule,',')) { // 长度区间
-                    list($min,$max) = explode(',',$rule);
-                    $check = ($length >= $min && $length <= $max);
+                    list($min,$max)   =  explode(',',$rule);
+                    return $length >= $min && $length <= $max;
                 }else{// 指定长度
-                    $check = ($length == $rule);
+                    return $length == $rule;
                 }
-                break;
             case 'expire':
-                list($start,$end) = explode(',',$rule);
-                if(!is_numeric($start)) $start = strtotime($start);
-                if(!is_numeric($end)) $end = strtotime($end);
-                $check = (NOW_TIME >= $start && NOW_TIME <= $end);
-                break;
+                list($start,$end)   =  explode(',',$rule);
+                if(!is_numeric($start)) $start   =  strtotime($start);
+                if(!is_numeric($end)) $end   =  strtotime($end);
+                return $_SERVER['REQUEST_TIME'] >= $start && $_SERVER['REQUEST_TIME'] <= $end;
             case 'ip_allow': // IP 操作许可验证
-                $check = in_array(get_client_ip(),explode(',',$rule));
-                break;
+                return in_array(get_client_ip(),explode(',',$rule));
             case 'ip_deny': // IP 操作禁止验证
-                $check = !in_array(get_client_ip(),explode(',',$rule));
-                break;
+                return !in_array(get_client_ip(),explode(',',$rule));
             case 'regex':
             default:    // 默认使用正则验证 可以使用验证类中定义的验证名称
                 // 检查附加规则
-                $check = $this->regex($value,$rule);
+                return $this->regex($value,$rule);
         }
-        return $reverse ? !$check : $check;
     }
 
     /**
