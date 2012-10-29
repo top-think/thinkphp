de6c6b397c08f126d26e81fbd2a7c66fcb5fe38e
thinkphp <thinkphp@qq.com>
2012-10-29 16:26:35 +0800

    改进import方法支持模式扩展未定义BASE_LIB_PATH的情况

--- ThinkPHP/Common/common.php
+++ ThinkPHP/Common/common.php
@@ -149,10 +149,11 @@ function import($class, $baseUrl = '', $ext='.class.php') {
         $_file[$class . $baseUrl] = true;
     $class_strut     = explode('/', $class);
     if (empty($baseUrl)) {
+        $libPath    =   defined('BASE_LIB_PATH')?BASE_LIB_PATH:LIB_PATH;
         if ('@' == $class_strut[0] || APP_NAME == $class_strut[0]) {
             //加载当前项目应用类库
-            $baseUrl = dirname(BASE_LIB_PATH);
-            $class   = substr_replace($class, basename(BASE_LIB_PATH).'/', 0, strlen($class_strut[0]) + 1);
+            $baseUrl = dirname($libPath);
+            $class   = substr_replace($class, basename($libPath).'/', 0, strlen($class_strut[0]) + 1);
         }elseif ('think' == strtolower($class_strut[0])){ // think 官方基类库
             $baseUrl = CORE_PATH;
             $class   = substr($class,6);
@@ -161,7 +162,7 @@ function import($class, $baseUrl = '', $ext='.class.php') {
             $baseUrl = LIBRARY_PATH;
         }else { // 加载其他项目应用类库
             $class   = substr_replace($class, '', 0, strlen($class_strut[0]) + 1);
-            $baseUrl = APP_PATH . '../' . $class_strut[0] . '/'.basename(BASE_LIB_PATH).'/';
+            $baseUrl = APP_PATH . '../' . $class_strut[0] . '/'.basename($libPath).'/';
         }
     }
     if (substr($baseUrl, -1) != '/')
