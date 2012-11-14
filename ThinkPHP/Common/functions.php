b1b3e7144ae8825148cc22a0f4ab11dede4a8dca
thinkphp <thinkphp@qq.com>
2012-11-14 09:39:56 +0800

    改进get_client_ip方法返回ipv4数字可能溢出的情况

--- ThinkPHP/Common/functions.php
+++ ThinkPHP/Common/functions.php
@@ -709,7 +709,7 @@ function get_client_ip($type = 0) {
         $ip     =   $_SERVER['REMOTE_ADDR'];
     }
     // IP地址合法验证
-    $long = ip2long($ip);
+    $long = sprintf("%u",ip2long($ip));
     $ip   = $long ? array($ip, $long) : array('0.0.0.0', 0);
     return $ip[$type];
 }
