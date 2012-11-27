1bf56a35555cca1e7fe7a8bcddfe382df5b40427
thinkphp <thinkphp@qq.com>
2012-11-27 16:18:52 +0800

    改进上传类

--- Extend/Library/ORG/Net/UploadFile.class.php
+++ Extend/Library/ORG/Net/UploadFile.class.php
@@ -94,7 +94,9 @@ class UploadFile {//类定义开始
             return false;
         }
         // 如果是图像文件 检测文件格式
-        if( in_array(strtolower($file['extension']),array('gif','jpg','jpeg','bmp','png','swf')) && false === getimagesize($file['tmp_name'])) {
+        if( in_array(strtolower($file['extension']),array('gif','jpg','jpeg','bmp','png','swf'))) {
+            $info   = getimagesize($file['tmp_name']);
+            if(false === $info || ('gif' == strtolower($file['extension']) && empty($info['bits'])))            
             $this->error = '非法图像文件';
             return false;
         }
