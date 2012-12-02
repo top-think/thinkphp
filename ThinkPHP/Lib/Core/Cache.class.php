27c867afddbfc0d77c6080443658b49301603c8b
thinkphp <thinkphp@qq.com>
2012-12-02 11:54:02 +0800

    改进cache类 支持定义队列名称 改进缓存更新对出列的影响 调试模式下面可以记录出列次数便于优化队列长度 增加对缓存类型其他方法的内部调用支持

--- ThinkPHP/Lib/Core/Cache.class.php
+++ ThinkPHP/Lib/Core/Cache.class.php
@@ -95,18 +95,33 @@ class Cache {
         );
         $queue  =  isset($this->options['queue'])?$this->options['queue']:'file';
         $fun    =  isset($_handler[$queue])?$_handler[$queue]:$_handler['file'];
-        $value  =  $fun[0]('think_queue');
+        $queue_name=isset($this->options['queue_name'])?$this->options['queue_name']:'think_queue';
+        $value  =  $fun[0]($queue_name);
         if(!$value) {
             $value   =  array();
         }
         // 进列
-        array_push($value,$key);
+        if(false===array_search($key, $value))  array_push($value,$key);
         if(count($value) > $this->options['length']) {
             // 出列
             $key =  array_shift($value);
             // 删除缓存
             $this->rm($key);
+             if(APP_DEUBG){
+                //调试模式下，记录出列次数
+                N($queue_name.'_out_times',1,true);
+            }
         }
-        return $fun[1]('think_queue',$value);
+        return $fun[1]($queue_name,$value);
+    }
+    
+    public function __call($method,$args){
+        //调用缓存类型自己的方法
+        if(method_exists($this->handler, $method)){
+           return call_user_func_array(array($this->handler,$method), $args);
+        }else{
+            throw_exception(__CLASS__.':'.$method.L('_METHOD_NOT_EXIST_'));
+            return;
+        }
     }
 }
