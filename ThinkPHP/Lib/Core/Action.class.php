3b31e1802730c823f0cec4862ee4a7e78e4a48e3
thinkphp <thinkphp@qq.com>
2012-12-03 10:23:44 +0800

    改进Action类中error和success方法中waitSecond参数设置为0无效的情况

--- ThinkPHP/Lib/Core/Action.class.php
+++ ThinkPHP/Lib/Core/Action.class.php
@@ -399,16 +399,16 @@ abstract class Action {
         if($status) { //发送成功信息
             $this->assign('message',$message);// 提示信息
             // 成功操作后默认停留1秒
-            if(!$this->get('waitSecond'))    $this->assign('waitSecond','1');
+            if(!isset($this->waitSecond))    $this->assign('waitSecond','1');
             // 默认操作成功自动返回操作前页面
-            if(!$this->get('jumpUrl')) $this->assign("jumpUrl",$_SERVER["HTTP_REFERER"]);
+            if(!isset($this->jumpUrl)) $this->assign("jumpUrl",$_SERVER["HTTP_REFERER"]);
             $this->display(C('TMPL_ACTION_SUCCESS'));
         }else{
             $this->assign('error',$message);// 提示信息
             //发生错误时候默认停留3秒
-            if(!$this->get('waitSecond'))    $this->assign('waitSecond','3');
+            if(!isset($this->waitSecond))    $this->assign('waitSecond','3');
             // 默认发生错误的话自动返回上页
-            if(!$this->get('jumpUrl')) $this->assign('jumpUrl',"javascript:history.back(-1);");
+            if(!isset($this->jumpUrl)) $this->assign('jumpUrl',"javascript:history.back(-1);");
             $this->display(C('TMPL_ACTION_ERROR'));
             // 中止执行  避免出错后继续执行
             exit ;
