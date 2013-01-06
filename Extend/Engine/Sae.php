02fbfcfff109ab37607a4b63bcdc254f104db5a1
thinkphp <thinkphp@qq.com>
2013-01-06 10:24:34 +0800

    sae引擎入口修改

--- Extend/Engine/Sae.php
+++ Extend/Engine/Sae.php
@@ -32,6 +32,16 @@ if(PHP_SAPI=='cli' && (!defined('MODE_NAME') || strtolower(MODE_NAME)!='cli')){
     exit();
 }
 //-------end 命令行生成缓存
+//记录开始运行时间
+$GLOBALS['_beginTime'] = microtime(TRUE);
+// 记录内存初始使用
+define('MEMORY_LIMIT_ON', function_exists('memory_get_usage'));
+if (MEMORY_LIMIT_ON) $GLOBALS['_startUseMems'] = memory_get_usage();
+// 系统目录定义
+defined('THINK_PATH') or define('THINK_PATH', dirname(dirname(dirname(__FILE__))) . '/');
+defined('APP_PATH') or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . '/');
+defined('RUNTIME_PATH') or define('RUNTIME_PATH', APP_PATH . 'Runtime/');
+defined('APP_DEBUG') or define('APP_DEBUG', false); // 是否调试模式
 //[sae]定义SAE_PATH
 defined('ENGINE_PATH') or define('ENGINE_PATH', dirname(__FILE__) . '/');
 define('SAE_PATH', ENGINE_PATH . 'Sae/');
@@ -39,7 +49,6 @@ define('SAE_PATH', ENGINE_PATH . 'Sae/');
 if (!function_exists('saeAutoLoader')) {
     define('IS_SAE', FALSE);
     defined('THINK_PATH') or define('THINK_PATH', dirname(dirname(dirname(__FILE__))) . '/');
-	defined('APP_PATH') or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . '/');
 	//创建默认项目
     $lib_path=defined('LIB_PATH')?LIB_PATH:APP_PATH.'Lib/';
 	if(!is_dir($lib_path)) require SAE_PATH.'build_first_app.php';
@@ -47,24 +56,24 @@ if (!function_exists('saeAutoLoader')) {
     require SAE_PATH . 'Common/sae_functions.php';
     //加载模拟器
     if (!defined('SAE_ACCESSKEY'))  require SAE_PATH . 'SaeImit.php';
-    require THINK_PATH . 'ThinkPHP.php';
+    $runtime = defined('MODE_NAME')?'~'.strtolower(MODE_NAME).'_runtime.php':'~runtime.php';
+    defined('RUNTIME_FILE') or define('RUNTIME_FILE',RUNTIME_PATH.$runtime);
+    if(!APP_DEBUG && is_file(RUNTIME_FILE)) {
+        // 部署模式直接载入运行缓存
+        require RUNTIME_FILE;
+    }else{
+        // 加载运行时文件
+        require THINK_PATH.'Common/runtime.php';
+    }       
 } else {
     define('IS_SAE', TRUE);
     defined('SAE_RUNTIME') or define('SAE_RUNTIME',false);
     require SAE_PATH . 'Lib/Core/SaeMC.class.php';
-    //记录开始运行时间
-    $GLOBALS['_beginTime'] = microtime(TRUE);
-    // 记录内存初始使用
-    define('MEMORY_LIMIT_ON', function_exists('memory_get_usage'));
-    if (MEMORY_LIMIT_ON) $GLOBALS['_startUseMems'] = memory_get_usage();
-    defined('APP_PATH') or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . '/');
     //[sae] 判断是否手动建立项目目录
     if (!is_dir(APP_PATH . '/Lib/')) {
         header('Content-Type:text/html; charset=utf-8');
         exit('<div style=\'font-weight:bold;float:left;width:430px;text-align:center;border:1px solid silver;background:#E8EFFF;padding:8px;color:red;font-size:14px;font-family:Tahoma\'>sae环境下请手动生成项目目录~</div>');
     }
-    defined('RUNTIME_PATH') or define('RUNTIME_PATH', APP_PATH . 'Runtime/');
-    defined('APP_DEBUG') or define('APP_DEBUG', false); // 是否调试模式
     $runtime = defined('MODE_NAME') ? '~' . strtolower(MODE_NAME) . '_sae_runtime.php' : '~sae_runtime.php';
     defined('RUNTIME_FILE') or define('RUNTIME_FILE', RUNTIME_PATH . $runtime);
     //[sae] 载入核心编译缓存
@@ -72,8 +81,6 @@ if (!function_exists('saeAutoLoader')) {
         // 部署模式直接载入allinone缓存
         SaeMC::include_file(RUNTIME_FILE);
     } else {
-        // ThinkPHP系统目录定义
-        defined('THINK_PATH') or define('THINK_PATH', dirname(dirname(dirname(__FILE__))) . '/');
         //[sae] 加载运行时文件
         require SAE_PATH . 'Common/runtime.php';
     }
