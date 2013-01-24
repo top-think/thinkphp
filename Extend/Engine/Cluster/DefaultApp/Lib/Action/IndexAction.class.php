27dc1d33c9dc412a651463f12b0b8e875e26df15
luofei614 <upfy@qq.com>
2013-01-24 18:00:35 +0800

    cluster引擎，生成的默认项目链接增加空格

--- Extend/Engine/Cluster/DefaultApp/Lib/Action/IndexAction.class.php
+++ Extend/Engine/Cluster/DefaultApp/Lib/Action/IndexAction.class.php
@@ -2,7 +2,7 @@
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
     public function index(){
-	$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b> (Cluster Engine for '.IO_NAME.')！</p></div><div>测试：<a href="__URL__/f" target="_blank">F函数</a> <a href="__URL__/s" target="_blank">S函数</a> <a href="__URL__/upload" target="_blank">上传图片</a> <a href="__URL__/log" target="_blank">日志记录</a></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+	$this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b> (Cluster Engine for '.IO_NAME.')！</p></div><div>测试：<a href="__URL__/f" target="_blank">F函数</a>&nbsp;<a href="__URL__/s" target="_blank">S函数</a>&nbsp;<a href="__URL__/upload" target="_blank">上传图片</a>&nbsp;<a href="__URL__/log" target="_blank">日志记录</a></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
 	}
 	public function f(){
 		F('name','success');
