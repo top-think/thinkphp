d3ac60caf488b48d90ba5e872ac770e8d6182cfd
MinoWu <minowu@foxmail.com>
2013-02-08 14:19:37 +0800

    修改thinkphp入口文件的注释。

--- ThinkPHP.php
+++ ThinkPHP.php
@@ -10,27 +10,41 @@
 // +----------------------------------------------------------------------
 
 // ThinkPHP 入口文件
+
 // 记录开始运行时间
 $GLOBALS['_beginTime'] = microtime(TRUE);
+
 // 记录内存初始使用
-define('MEMORY_LIMIT_ON',function_exists('memory_get_usage'));
+define('MEMORY_LIMIT_ON', function_exists('memory_get_usage'));
 if(MEMORY_LIMIT_ON) $GLOBALS['_startUseMems'] = memory_get_usage();
-// 系统目录定义
-defined('THINK_PATH') 	or define('THINK_PATH', dirname(__FILE__).'/');
-defined('APP_PATH') 	or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']).'/');
-defined('RUNTIME_PATH') or define('RUNTIME_PATH',APP_PATH.'Runtime/');
-defined('APP_DEBUG') 	or define('APP_DEBUG',false); // 是否调试模式
+
+// Thinkphp框架目录
+// 项目目录
+// 临时运行文件目录
+defined('THINK_PATH') 	or define('THINK_PATH', dirname(__FILE__) . '/');
+defined('APP_PATH') 	or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . '/');
+defined('RUNTIME_PATH') or define('RUNTIME_PATH', APP_PATH . 'Runtime/');
+
+// 是否开启调试模式
+defined('APP_DEBUG') 	or define('APP_DEBUG', false);
+
+// 运行
 if(defined('ENGINE_NAME')) {
+
     defined('ENGINE_PATH') or define('ENGINE_PATH',THINK_PATH.'Extend/Engine/');
 	require ENGINE_PATH.strtolower(ENGINE_NAME).'.php';
 }else{
+
 	$runtime = defined('MODE_NAME')?'~'.strtolower(MODE_NAME).'_runtime.php':'~runtime.php';
 	defined('RUNTIME_FILE') or define('RUNTIME_FILE',RUNTIME_PATH.$runtime);
+
 	if(!APP_DEBUG && is_file(RUNTIME_FILE)) {
-	    // 部署模式直接载入运行缓存
+
+	    // 生产环境，直接载入生成的运行文件
 	    require RUNTIME_FILE;
 	}else{
-	    // 加载运行时文件
+
+	    // 开发环境
 	    require THINK_PATH.'Common/runtime.php';
 	}	
 }
