103768e095eecb59a0d6f4dce6d9c1fa62afa27d
MinoWu <minowu@foxmail.com>
2013-02-08 14:56:39 +0800

    在'URL_CASE_INSENSITIVE'为true的模式下，__GROUP__定义为小写。

--- Lib/Core/Dispatcher.class.php
+++ Lib/Core/Dispatcher.class.php
@@ -132,7 +132,7 @@ class Dispatcher {
         if (C('APP_GROUP_LIST')) {
             define('GROUP_NAME', self::getGroup(C('VAR_GROUP')));
             // 分组URL地址
-            define('__GROUP__',(!empty($domainGroup) || strtolower(GROUP_NAME) == strtolower(C('DEFAULT_GROUP')) )?__APP__ : __APP__.'/'.GROUP_NAME);
+            define('__GROUP__', (!empty($domainGroup) || strtolower(GROUP_NAME) == strtolower(C('DEFAULT_GROUP')) ) ? __APP__ : __APP__ . '/' . (C('URL_CASE_INSENSITIVE') ? strtolower(GROUP_NAME) : GROUP_NAME) );
         }
         
         // 定义项目基础加载路径
