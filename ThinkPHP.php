8e652d2979dd7b099260625a6909c1d03e89f8c9
MinoWu <minowu@foxmail.com>
2013-02-08 14:37:45 +0800

    将runtime.php文件里的EXTEND_PATH的定义移到ThinkPHP.php文件内。

--- ThinkPHP.php
+++ ThinkPHP.php
@@ -9,7 +9,7 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-// ThinkPHP 入口文件
+// Thinkphp框架入口
 
 // 记录开始运行时间
 $GLOBALS['_beginTime'] = microtime(TRUE);
@@ -19,25 +19,33 @@ define('MEMORY_LIMIT_ON', function_exists('memory_get_usage'));
 if(MEMORY_LIMIT_ON) $GLOBALS['_startUseMems'] = memory_get_usage();
 
 // Thinkphp框架目录
+// Thinkphp扩展目录
 // 项目目录
 // 临时运行文件目录
 defined('THINK_PATH') 	or define('THINK_PATH', dirname(__FILE__) . '/');
+defined('EXTEND_PATH')  or define('EXTEND_PATH', THINK_PATH . 'Extend/');
 defined('APP_PATH') 	or define('APP_PATH', dirname($_SERVER['SCRIPT_FILENAME']) . '/');
 defined('RUNTIME_PATH') or define('RUNTIME_PATH', APP_PATH . 'Runtime/');
 
 // 是否开启调试模式
 defined('APP_DEBUG') 	or define('APP_DEBUG', false);
 
-// 运行
+// 运行模式，是否使用sae等引擎
 if(defined('ENGINE_NAME')) {
 
-    defined('ENGINE_PATH') or define('ENGINE_PATH',THINK_PATH.'Extend/Engine/');
-	require ENGINE_PATH.strtolower(ENGINE_NAME).'.php';
-}else{
+	// 第三方引擎运行
+	defined('ENGINE_PATH') or define('ENGINE_PATH', EXTEND_PATH . 'Engine/');
+	require ENGINE_PATH . strtolower(ENGINE_NAME) . '.php';
+} else {
 
-	$runtime = defined('MODE_NAME')?'~'.strtolower(MODE_NAME).'_runtime.php':'~runtime.php';
-	defined('RUNTIME_FILE') or define('RUNTIME_FILE',RUNTIME_PATH.$runtime);
+	// 普通方式运行
 
+	// 应用运行模式和定义RUNTIME_FILE
+	// MODE_NAME, Cli, Lite, Thin, AMF, PHPPRC, REST
+	$runtime = defined('MODE_NAME') ? '~' . strtolower(MODE_NAME) . '_runtime.php' : '~runtime.php';
+	defined('RUNTIME_FILE') or define('RUNTIME_FILE', RUNTIME_PATH . $runtime);
+
+	// 开始运行
 	if(!APP_DEBUG && is_file(RUNTIME_FILE)) {
 
 	    // 生产环境，直接载入生成的运行文件
@@ -46,5 +54,5 @@ if(defined('ENGINE_NAME')) {
 
 	    // 开发环境
 	    require THINK_PATH.'Common/runtime.php';
-	}	
+	}
 }
