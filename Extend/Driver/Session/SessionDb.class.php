0fb5afa8f9992954f1b1a7cb7c17fda257625841
YanSu <yansu0711@gmail.com>
2013-02-22 18:15:04 +0800

    添加主从分离
    增加主从分离

--- Extend/Driver/Session/SessionDb.class.php
+++ Extend/Driver/Session/SessionDb.class.php
@@ -38,7 +38,7 @@ class SessionDb {
     /**
      * 数据库句柄
      */
-   protected $hander; 
+   protected $hander  = array(); 
 
     /**
      * 打开Session 
@@ -49,8 +49,58 @@ class SessionDb {
     public function open($savePath, $sessName) { 
        $this->lifeTime = C('SESSION_EXPIRE')?C('SESSION_EXPIRE'):ini_get('session.gc_maxlifetime');
        $this->sessionTable  =   C('SESSION_TABLE')?C('SESSION_TABLE'):C("DB_PREFIX")."session";
-       $hander  = mysql_connect(C('DB_HOST'),C('DB_USER'),C('DB_PWD')); 
-       $dbSel   = mysql_select_db(C('DB_NAME'),$hander);
+       //分布式数据库
+       $host = explode(',',C('DB_HOST'));
+       $port = explode(',',C('DB_PORT'));
+       $name = explode(',',C('DB_NAME'));
+       $user = explode(',',C('DB_USER'));
+       $pwd  = explode(',',C('DB_PWD'));
+       if(1 == C('DB_DEPLOY_TYPE')){
+           //读写分离
+           if(C('DB_RW_SEPARATE')){
+               $w = floor(mt_rand(0,C('DB_MASTER_NUM')-1));
+               if(is_numeric(C('DB_SLAVE_NO'))){//指定服务器读
+                   $r = C('DB_SLAVE_NO');
+               }else{
+                   $r = floor(mt_rand(C('DB_MASTER_NUM'),count($host)-1));
+               }
+               //主数据库链接
+               $hander = mysql_connect(
+                   $host[$w].(isset($port[$w])?':'.$port[$w]:':'.$port[0]),
+                   isset($user[$w])?$user[$w]:$user[0],
+                   isset($pwd[$w])?$pwd[$w]:$pwd[0]
+                   );
+               $dbSel = mysql_select_db(
+                   isset($name[$w])?$name[$w]:$name[0]
+                   ,$hander);
+               if(!$hander || !$dbSel)
+                   return false;
+               $this->hander[0] = $hander;
+               //从数据库链接
+               $hander = mysql_connect(
+                   $host[$r].(isset($port[$r])?':'.$port[$r]:':'.$port[0]),
+                   isset($user[$r])?$user[$r]:$user[0],
+                   isset($pwd[$r])?$pwd[$r]:$pwd[0]
+                   );
+               $dbSel = mysql_select_db(
+                   isset($name[$r])?$name[$r]:$name[0]
+                   ,$hander);
+               if(!$hander || !$dbSel)
+                   return false;
+               $this->hander[1] = $hander;
+               return true;
+           }
+       }
+       //从数据库链接
+       $r = floor(mt_rand(0,count($host)-1));
+       $hander = mysql_connect(
+           $host[$r].(isset($port[$r])?':'.$port[$r]:':'.$port[0]),
+           isset($user[$r])?$user[$r]:$user[0],
+           isset($pwd[$r])?$pwd[$r]:$pwd[0]
+           );
+       $dbSel = mysql_select_db(
+           isset($name[$r])?$name[$r]:$name[0]
+           ,$hander);
        if(!$hander || !$dbSel) 
            return false; 
        $this->hander = $hander; 
@@ -61,8 +111,12 @@ class SessionDb {
      * 关闭Session 
      * @access public 
      */
-   public function close() { 
-       $this->gc(ini_get('session.gc_maxlifetime')); 
+   public function close() {
+       if(is_array($this->hander)){
+           $this->gc($this->lifeTime);
+           return (mysql_close($this->hander[0]) && mysql_close($this->hander[1]));
+       }
+       $this->gc($this->lifeTime); 
        return mysql_close($this->hander); 
    } 
 
@@ -72,7 +126,8 @@ class SessionDb {
      * @param string $sessID 
      */
    public function read($sessID) { 
-       $res = mysql_query("SELECT session_data AS data FROM ".$this->sessionTable." WHERE session_id = '$sessID'   AND session_expire >".time(),$this->hander); 
+       $hander = is_array($this->hander)?$this->hander[1]:$this->hander;
+       $res = mysql_query("SELECT session_data AS data FROM ".$this->sessionTable." WHERE session_id = '$sessID'   AND session_expire >".time(),$hander); 
        if($res) {
            $row = mysql_fetch_assoc($res);
            return $row['data']; 
@@ -87,9 +142,10 @@ class SessionDb {
      * @param String $sessData  
      */
    public function write($sessID,$sessData) { 
+       $hander = is_array($this->hander)?$this->hander[0]:$this->hander;
        $expire = time() + $this->lifeTime; 
-       mysql_query("REPLACE INTO  ".$this->sessionTable." (  session_id, session_expire, session_data)  VALUES( '$sessID', '$expire',  '$sessData')",$this->hander); 
-       if(mysql_affected_rows($this->hander)) 
+       mysql_query("REPLACE INTO  ".$this->sessionTable." (  session_id, session_expire, session_data)  VALUES( '$sessID', '$expire',  '$sessData')",$hander); 
+       if(mysql_affected_rows($hander)) 
            return true; 
        return false; 
    } 
@@ -100,8 +156,9 @@ class SessionDb {
      * @param string $sessID 
      */
    public function destroy($sessID) { 
-       mysql_query("DELETE FROM ".$this->sessionTable." WHERE session_id = '$sessID'",$this->hander); 
-       if(mysql_affected_rows($this->hander)) 
+       $hander = is_array($this->hander)?$this->hander[0]:$this->hander;
+       mysql_query("DELETE FROM ".$this->sessionTable." WHERE session_id = '$sessID'",$hander); 
+       if(mysql_affected_rows($hander)) 
            return true; 
        return false; 
    } 
@@ -112,8 +169,9 @@ class SessionDb {
      * @param string $sessMaxLifeTime 
      */
    public function gc($sessMaxLifeTime) { 
-       mysql_query("DELETE FROM ".$this->sessionTable." WHERE session_expire < ".time(),$this->hander); 
-       return mysql_affected_rows($this->hander); 
+       $hander = is_array($this->hander)?$this->hander[0]:$this->hander;
+       mysql_query("DELETE FROM ".$this->sessionTable." WHERE session_expire < ".time(),$hander); 
+       return mysql_affected_rows($hander); 
    } 
 
     /**
@@ -121,7 +179,7 @@ class SessionDb {
      * @access public 
      */
     public function execute() {
-    	session_set_save_handler(array(&$this,"open"), 
+        session_set_save_handler(array(&$this,"open"), 
                          array(&$this,"close"), 
                          array(&$this,"read"), 
                          array(&$this,"write"), 
