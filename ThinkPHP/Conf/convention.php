ca26d6e1d4d59762460b24a420f69c935bdfa036
VikeyChen <250677900@qq.com>
2013-03-29 10:46:18 +0800

    Revert "Add Official Extend"
    This reverts commit 666c155e95e9c6c40bd37f1cc96f6d7238caafd0.

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -58,9 +58,9 @@ return  array(
     /* 数据库设置 */
     'DB_TYPE'               => 'mysql',     // 数据库类型
     'DB_HOST'               => 'localhost', // 服务器地址
-    'DB_NAME'               => 'thinkphp',          // 数据库名
+    'DB_NAME'               => '',          // 数据库名
     'DB_USER'               => 'root',      // 用户名
-    'DB_PWD'                => 'dddddd',          // 密码
+    'DB_PWD'                => '',          // 密码
     'DB_PORT'               => '',        // 端口
     'DB_PREFIX'             => 'think_',    // 数据库表前缀
     'DB_FIELDTYPE_CHECK'    => false,       // 是否进行字段类型检查
--- ThinkPHP/Extend/Engine/Sae/DefaultApp/Conf/config_sae.php
+++
@@ -1,7 +0,0 @@
-<?php
-return array(
-   'TMPL_PARSE_STRING'=>array(
-      // __PUBLIC__/upload  -->  /Public/upload -->http://appname-public.stor.sinaapp.com/upload
-   '/Public/upload'=>sae_storage_root('Public').'/upload'
-    )
-);
