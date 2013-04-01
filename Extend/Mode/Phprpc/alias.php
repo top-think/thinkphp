7a27201ca61a729d483e407626ec95a2a49dd2b0
thinkphp <thinkphp@qq.com>
2013-04-01 15:02:33 +0800

    phprpc模式扩展的alias文件调整

--- Extend/Mode/Phprpc/alias.php
+++ Extend/Mode/Phprpc/alias.php
@@ -11,7 +11,7 @@
 
 // 导入别名定义
 alias_import(array(
-    'Model'         =>   MODE_PATH.'Amf/Model.class.php',
+    'Model'         =>   MODE_PATH.'Phprpc/Model.class.php',
     'Db'                  =>    MODE_PATH.'Phprpc/Db.class.php',
     'Debug'              =>    CORE_PATH.'Util/Debug.class.php',
     'Session'             =>   CORE_PATH.'Util/Session.class.php',
