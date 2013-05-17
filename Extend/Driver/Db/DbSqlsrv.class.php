1248c23462ffebbda31ec2d9fe6e9db482174edd
麦当苗儿 <zuojiazi@vip.qq.com>
2013-05-17 14:30:35 +0800

    sqlsrv驱动添加自动获取主键字段

--- Extend/Driver/Db/DbSqlsrv.class.php
+++ Extend/Driver/Db/DbSqlsrv.class.php
@@ -204,14 +204,16 @@ class DbSqlsrv extends Db{
      * @return array
      */
     public function getFields($tableName) {
-        $result =   $this->query("SELECT   column_name,   data_type,   column_default,   is_nullable
-        FROM    information_schema.tables AS t
-        JOIN    information_schema.columns AS c
-        ON  t.table_catalog = c.table_catalog
-        AND t.table_schema  = c.table_schema
-        AND t.table_name    = c.table_name
-        WHERE   t.table_name = '$tableName'");
-        $info   =   array();
+        $result = $this->query("
+            SELECT column_name,data_type,column_default,is_nullable
+            FROM   information_schema.tables AS t
+            JOIN   information_schema.columns AS c
+            ON     t.table_catalog = c.table_catalog
+            AND    t.table_schema  = c.table_schema
+            AND    t.table_name    = c.table_name
+            WHERE  t.table_name = '{$tableName}'");
+        $pk = $this->query("SELECT * FROM INFORMATION_SCHEMA.KEY_COLUMN_USAGE WHERE TABLE_NAME='{$tableName}'");
+        $info = array();
         if($result) {
             foreach ($result as $key => $val) {
                 $info[$val['column_name']] = array(
@@ -219,7 +221,7 @@ class DbSqlsrv extends Db{
                     'type'    => $val['data_type'],
                     'notnull' => (bool) ($val['is_nullable'] === ''), // not null is empty, null is yes
                     'default' => $val['column_default'],
-                    'primary' => false,
+                    'primary' => $val['column_name'] == $pk[0]['COLUMN_NAME'],
                     'autoinc' => false,
                 );
             }
