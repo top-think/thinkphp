4cb0234df473b4affe0a774d48c127a6ab7564bc
ThinkPHP <thinkphp@qq.com>
2013-09-13 15:06:31 +0800

    普通模式文件核心 加入行为扩展类

--- ThinkPHP/Conf/Mode/common.php
+++ ThinkPHP/Conf/Mode/common.php
@@ -47,7 +47,13 @@ return array(
 		CORE_PATH . 'Log'.EXT,
 		CORE_PATH . 'Route'.EXT,
 		CORE_PATH . 'Action'.EXT,
-		CORE_PATH . 'View'.EXT
+		CORE_PATH . 'View'.EXT,
+		CORE_PATH . 'Behavior/ReadHtmlCacheBehavior'.EXT,
+		CORE_PATH . 'Behavior/ShowPageTraceBehavior'.EXT,
+		CORE_PATH . 'Behavior/ParseTemplateBehavior'.EXT,
+		CORE_PATH . 'Behavior/ContentReplaceBehavior'.EXT,
+		CORE_PATH . 'Behavior/TokenBuildBehavior'.EXT,		
+		CORE_PATH . 'Behavior/WriteHtmlCacheBehavior'.EXT,		
 	),
 	// 行为扩展定义
 	'extends'	=>	array(
