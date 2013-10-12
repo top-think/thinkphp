9266d6aa389b6df0d08111a7d5aef41a50cccfef
luofei614 <upfy@qq.com>
2013-10-12 18:14:07 +0800

    修复ChromeShowPageTrace的bug

--- ThinkPHP/Library/Think/Behavior/ChromeShowPageTraceBehavior.class.php
+++ ThinkPHP/Library/Think/Behavior/ChromeShowPageTraceBehavior.class.php
@@ -48,7 +48,7 @@ class ChromeShowPageTraceBehavior extends Behavior {
 
     // 行为扩展的执行入口必须是run
     public function run(&$params){
-            if(C('CHROME_SHOW_PAGE_TRACE')) $this->showTrace();
+            if(C('SHOW_PAGE_TRACE')) $this->showTrace();
     }
 
    
