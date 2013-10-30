ae46f108bb116725b026813288a184a0ed1af1df
ThinkPHP <thinkphp@qq.com>
2013-10-30 20:59:40 +0800

    调整

--- ThinkPHP/Common/build.php
+++ ThinkPHP/Common/build.php
@@ -87,7 +87,7 @@ function build_first_action() {
 // 生成目录安全文件
 function build_dir_secure($dirs=array()) {
     // 目录安全写入（默认开启）
-    defined('BUILD_DIR_SECURE')  or define('BUILD_DIR_SECURE',    false);
+    defined('BUILD_DIR_SECURE')  or define('BUILD_DIR_SECURE',    true);
     if(BUILD_DIR_SECURE) {
         defined('DIR_SECURE_FILENAME')  or define('DIR_SECURE_FILENAME',    'index.html');
         defined('DIR_SECURE_CONTENT')   or define('DIR_SECURE_CONTENT',     ' ');
