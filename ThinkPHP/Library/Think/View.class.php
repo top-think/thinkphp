92a8eeb8f0bd9a610655c85a0a4cc1eea35a3157
ThinkPHP <thinkphp@qq.com>
2013-11-27 22:53:01 +0800

    View类改进 支持找不到当前主题模板的时候定位默认主题中的模板

--- ThinkPHP/Library/Think/View.class.php
+++ ThinkPHP/Library/Think/View.class.php
@@ -166,7 +166,12 @@ class View {
         }elseif(false === strpos($template, $depr)){
             $template = CONTROLLER_NAME . $depr . $template;
         }
-        return THEME_PATH.$template.C('TMPL_TEMPLATE_SUFFIX');
+        $file   =   THEME_PATH.$template.C('TMPL_TEMPLATE_SUFFIX');
+        if(THEME_NAME != C('DEFAULT_THEME') && !is_file($file)){
+            // 找不到当前主题模板的时候定位默认主题中的模板
+            $file   =   dirname(THEME_PATH).'/'.C('DEFAULT_THEME').$template.C('TMPL_TEMPLATE_SUFFIX');
+        }
+        return $file;
     }
 
     /**
