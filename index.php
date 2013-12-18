676637d12b3aa2eadd738d290a5adfd0814132f0
ThinkPHP <thinkphp@qq.com>
2013-12-18 12:25:52 +0800

    应用入口文件注释修改

--- index.php
+++ index.php
@@ -9,8 +9,15 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-// ThinkPHP 入口文件
+// 应用入口文件
+
+// 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
+// 定义应用目录
 define('APP_PATH','./Application/');
+
+// 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
+
+// 亲^_^ 后面不需要任何代码了 就是如此简单
