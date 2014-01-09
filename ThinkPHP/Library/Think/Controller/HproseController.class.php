ffd91bbb4d36ed8f13a85b14acff6deae74fd196
ThinkPHP <thinkphp@qq.com>
2014-01-09 18:33:51 +0800

    hprose和rpc控制器添加__call方法 兼容App类找不到操作方法调用的异常

--- ThinkPHP/Library/Think/Controller/HproseController.class.php
+++ ThinkPHP/Library/Think/Controller/HproseController.class.php
@@ -49,4 +49,12 @@ class HproseController {
         $server->start();
     }
 
+    /**
+     * 魔术方法 有不存在的操作的时候执行
+     * @access public
+     * @param string $method 方法名
+     * @param array $args 参数
+     * @return mixed
+     */
+    public function __call($method,$args){}
 }
--- ThinkPHP/Library/Think/Controller/RpcController.class.php
+++ ThinkPHP/Library/Think/Controller/RpcController.class.php
@@ -44,4 +44,12 @@ class RpcController {
         echo $server->comment();
     }
 
+    /**
+     * 魔术方法 有不存在的操作的时候执行
+     * @access public
+     * @param string $method 方法名
+     * @param array $args 参数
+     * @return mixed
+     */
+    public function __call($method,$args){}
 }
