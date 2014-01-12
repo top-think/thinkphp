f295e5e231313d5faa1a9a819e50b30fc1e488dd
ThinkPHP <thinkphp@qq.com>
2014-01-12 12:05:37 +0800

    上传类改进

--- ThinkPHP/Library/Think/Upload.class.php
+++ ThinkPHP/Library/Think/Upload.class.php
@@ -15,18 +15,20 @@ class Upload{
 	 * @var array
 	 */
     private $config = array(
-        'mimes'    => array(), //允许上传的文件MiMe类型
-        'maxSize'  => 0, //上传的文件大小限制 (0-不做限制)
-        'exts'     => array(), //允许上传的文件后缀
-        'autoSub'  => true, //自动子目录保存文件
-        'subName'  => array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
-    	'rootPath' => './Uploads/', //保存根路径
-        'savePath' => '', //保存路径
-        'saveName' => array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
-        'saveExt'  => '', //文件保存后缀，空则使用原后缀
-        'replace'  => false, //存在同名是否覆盖
-        'hash'     => true, //是否生成hash编码
-        'callback' => false, //检测文件是否存在回调，如果存在返回文件信息数组
+        'mimes'         =>  array(), //允许上传的文件MiMe类型
+        'maxSize'       =>  0, //上传的文件大小限制 (0-不做限制)
+        'exts'          =>  array(), //允许上传的文件后缀
+        'autoSub'       =>  true, //自动子目录保存文件
+        'subName'       =>  array('date', 'Y-m-d'), //子目录创建方式，[0]-函数名，[1]-参数，多个参数使用数组
+    	'rootPath'      =>     './Uploads/', //保存根路径
+        'savePath'      =>  '', //保存路径
+        'saveName'      =>  array('uniqid', ''), //上传文件命名规则，[0]-函数名，[1]-参数，多个参数使用数组
+        'saveExt'       =>  '', //文件保存后缀，空则使用原后缀
+        'replace'       =>  false, //存在同名是否覆盖
+        'hash'          =>  true, //是否生成hash编码
+        'callback'      =>  false, //检测文件是否存在回调，如果存在返回文件信息数组
+        'driver'        =>  '', // 文件上传驱动
+        'driverConfig'  =>  array(), // 上传驱动配置
     );
 
     /**
@@ -48,8 +50,9 @@ class Upload{
      */
     public function __construct($config = array(), $driver = '', $driverConfig = null){
     	/* 获取配置 */
-        $this->config = array_merge($this->config, $config);
-        $driver     =   $driver? $driver : C('FILE_UPLOAD_TYPE');
+        $this->config   =   array_merge($this->config, $config);
+        $driver         =   $driver? $driver : ($this->driver? $this->driver : C('FILE_UPLOAD_TYPE'));
+        $driverConfig   =   $driverConfig? $driverConfig : ($this->driverConfig? $this->driverConfig :C('UPLOAD_TYPE_CONFIG'));
 
         /* 设置上传驱动 */
         $class      =   strpos($driver,'\\')? $driver : 'Think\\Upload\\Driver\\'.ucfirst(strtolower($driver));
