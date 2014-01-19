a6eb8927f1b5aa6a92131e39fc09d36a158f6981
ThinkPHP <thinkphp@qq.com>
2014-01-19 16:14:24 +0800

    默认URL不区分大小写

--- ThinkPHP/Conf/debug.php
+++ ThinkPHP/Conf/debug.php
@@ -24,4 +24,5 @@ return  array(
     'TMPL_CACHE_ON'         =>  false,        // 是否开启模板编译缓存,设为false则每次都会重新编译
     'TMPL_STRIP_SPACE'      =>  false,       // 是否去除模板文件里面的html空格与换行
     'SHOW_ERROR_MSG'        =>  true,    // 显示错误信息
+    'URL_CASE_INSENSITIVE'  =>  false,		 // URL区分大小写
 );
