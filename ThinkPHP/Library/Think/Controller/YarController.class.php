ec4903dbfdd6adaa1e0188bf7970d2ba7322c9fe
ThinkPHP <thinkphp@qq.com>
2014-01-20 16:47:35 +0800

    增加Yar扩展支持

---
+++ ThinkPHP/Library/Think/Controller/YarController.class.php
@@ -0,0 +1,42 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2013 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+namespace Think\Controller;
+/**
+ * ThinkPHP Hprose控制器类
+ */
+class YarController {
+
+   /**
+     * 架构函数
+     * @access public
+     */
+    public function __construct() {
+        //控制器初始化
+        if(method_exists($this,'_initialize'))
+            $this->_initialize();
+        //判断扩展是否存在
+        if(!extension_loaded('yar'))
+            E(L('_NOT_SUPPERT_').':yar');
+        //实例化phprpc
+        $server     =   new \Yar_Server($this);
+        // 启动server
+        $server->handle();
+    }
+
+    /**
+     * 魔术方法 有不存在的操作的时候执行
+     * @access public
+     * @param string $method 方法名
+     * @param array $args 参数
+     * @return mixed
+     */
+    public function __call($method,$args){}
+}
