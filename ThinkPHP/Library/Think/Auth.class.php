ec29aaa7caa2fb267503e82ce531a98adb9a4018
ThinkPHP <thinkphp@qq.com>
2014-01-31 13:36:09 +0800

    Auth类的sql调整

--- ThinkPHP/Library/Think/Auth.class.php
+++ ThinkPHP/Library/Think/Auth.class.php
@@ -34,6 +34,7 @@ CREATE TABLE `think_auth_rule` (
     `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,  
     `name` char(80) NOT NULL DEFAULT '',  
     `title` char(20) NOT NULL DEFAULT '',  
+    `type` tinyint(1) NOT NULL DEFAULT '1',    
     `status` tinyint(1) NOT NULL DEFAULT '1',  
     `condition` char(100) NOT NULL DEFAULT '',  # 规则附件条件,满足附加条件的规则,才认为是有效的规则
     PRIMARY KEY (`id`),  
