0b0c43ba8d7fbfcac76783ffcffe694c10d55bdb
ThinkPHP <thinkphp@qq.com>
2014-02-02 10:00:02 +0800

    C函数并入系统函数库文件

--- ThinkPHP/ThinkPHP.php
+++ ThinkPHP/ThinkPHP.php
@@ -88,48 +88,6 @@ if(!IS_CLI) {
     }
 }
 
-/**
- * 获取和设置配置参数 支持批量定义
- * @param string|array $name 配置变量
- * @param mixed $value 配置值
- * @return mixed
- */
-function C($name=null, $value=null) {
-    static $_config = array();
-    // 无参数时获取所有
-    if (empty($name)) {
-        if(!empty($value) && $array = S('c_'.$value)) {
-            $_config = array_merge($_config, array_change_key_case($array));
-        }
-        return $_config;
-    }
-    // 优先执行设置获取或赋值
-    if (is_string($name)) {
-        if (!strpos($name, '.')) {
-            $name = strtolower($name);
-            if (is_null($value))
-                return isset($_config[$name]) ? $_config[$name] : null;
-            $_config[$name] = $value;
-            return;
-        }
-        // 二维数组设置和获取支持
-        $name = explode('.', $name);
-        $name[0]   =  strtolower($name[0]);
-        if (is_null($value))
-            return isset($_config[$name[0]][$name[1]]) ? $_config[$name[0]][$name[1]] : null;
-        $_config[$name[0]][$name[1]] = $value;
-        return;
-    }
-    // 批量设置
-    if (is_array($name)){
-        $_config = array_merge($_config, array_change_key_case($name));
-        if(!empty($value)) {// 保存配置值
-            S('c_'.$value,$_config);
-        }
-        return;
-    }
-    return null; // 避免非法参数
-}
 // 加载核心Think类
 require CORE_PATH.'Think'.EXT;
 // 应用初始化 
