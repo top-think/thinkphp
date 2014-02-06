e809470aea63867d07c420d40f4c5993fbfa94ab
ThinkPHP <thinkphp@qq.com>
2014-02-06 14:53:17 +0800

    版本号升级为3.2.1

--- ThinkPHP/ThinkPHP.php
+++ ThinkPHP/ThinkPHP.php
@@ -20,7 +20,7 @@ define('MEMORY_LIMIT_ON',function_exists('memory_get_usage'));
 if(MEMORY_LIMIT_ON) $GLOBALS['_startUseMems'] = memory_get_usage();
 
 // 版本信息
-const THINK_VERSION     =   '3.2.0';
+const THINK_VERSION     =   '3.2.1';
 
 // URL 模式定义
 const URL_COMMON        =   0;  //普通模式
