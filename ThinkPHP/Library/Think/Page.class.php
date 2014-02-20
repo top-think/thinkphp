88290f66b32e8d4f2715329f6135db91315767ed
thinkphp <thinkphp@qq.com>
2014-02-20 17:05:43 +0800

    Page类添加默认的listRows值

--- ThinkPHP/Library/Think/Page.class.php
+++ ThinkPHP/Library/Think/Page.class.php
@@ -39,7 +39,7 @@ class Page{
      * @param array $listRows  每页显示记录数
      * @param array $parameter  分页跳转的参数
      */
-    public function __construct($totalRows, $listRows, $parameter = array()) {
+    public function __construct($totalRows, $listRows=20, $parameter = array()) {
         C('VAR_PAGE') && $this->p = C('VAR_PAGE'); //设置分页参数名称
         /* 基础设置 */
         $this->totalRows  = $totalRows; //设置总记录数
