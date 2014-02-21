09a177dcd89b90060c7e3c39bf6791959c195ca8
thinkphp <thinkphp@qq.com>
2014-02-21 15:23:57 +0800

    mysqli驱动的free方法修正

--- ThinkPHP/Library/Think/Db/Driver/Mysqli.class.php
+++ ThinkPHP/Library/Think/Db/Driver/Mysqli.class.php
@@ -64,7 +64,9 @@ class Mysqli extends Db{
      * @access public
      */
     public function free() {
-        $this->queryID->free_result();
+        if(is_object($this->queryID)){
+            $this->queryID->free_result();
+        }
         $this->queryID = null;
     }
 
