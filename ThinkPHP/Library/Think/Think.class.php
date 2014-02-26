305a2b0695275eea459def10129198aa7efea606
ThinkPHP <thinkphp@qq.com>
2014-02-26 21:00:15 +0800

    改进

--- ThinkPHP/Library/Think/Think.class.php
+++ ThinkPHP/Library/Think/Think.class.php
@@ -61,7 +61,7 @@ class Think {
 
           // 读取当前应用模式对应的配置文件
           if('common' != APP_MODE && is_file(CONF_PATH.'config_'.APP_MODE.CONF_EXT))
-              C(include CONF_PATH.'config_'.APP_MODE.CONF_EXT);  
+              C(load_config(CONF_PATH.'config_'.APP_MODE.CONF_EXT));  
 
           // 加载模式别名定义
           if(isset($mode['alias'])){
