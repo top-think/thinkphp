6215987567156397d711b97c44fdf000487442c0
heui <runphp@qq.com>
2014-03-14 15:56:49 +0800

    TOKEN_NAME 没设置时使用默认值
    TOKEN_NAME 没设置时使用默认值

--- ThinkPHP/Library/Think/Model.class.php
+++ ThinkPHP/Library/Think/Model.class.php
@@ -915,7 +915,7 @@ class Model {
         // 支持使用token(false) 关闭令牌验证
         if(isset($this->options['token']) && !$this->options['token']) return true;
         if(C('TOKEN_ON')){
-            $name   = C('TOKEN_NAME');
+            $name   = C('TOKEN_NAME', null, '__hash__');
             if(!isset($data[$name]) || !isset($_SESSION[$name])) { // 令牌数据无效
                 return false;
             }
