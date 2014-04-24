0da238d1c061a813b3ecde5ae5ad5f9ac82d3039
thinkphp <thinkphp@qq.com>
2014-04-24 15:55:02 +0800

    Image类的imagick驱动save方法增加quality参数 用于图片质量设置，默认值为80

--- ThinkPHP/Library/Think/Image/Driver/Imagick.class.php
+++ ThinkPHP/Library/Think/Image/Driver/Imagick.class.php
@@ -60,9 +60,10 @@ class Imagick{
      * 保存图像
      * @param  string  $imgname   图像保存名称
      * @param  string  $type      图像类型
+     * @param  integer $quality   JPEG图像质量      
      * @param  boolean $interlace 是否对JPEG类型图像设置隔行扫描
      */
-    public function save($imgname, $type = null, $interlace = true){
+    public function save($imgname, $type = null, $quality=80,$interlace = true){
         if(empty($this->img)) E('没有可以被保存的图像资源');
 
         //设置图片类型
@@ -77,6 +78,9 @@ class Imagick{
         if('jpeg' == $type || 'jpg' == $type){
             $this->img->setImageInterlaceScheme(1);
         }
+
+        // 设置图像质量
+        $this->img->setImageCompressionQuality($quality); 
 
         //去除图像配置信息
         $this->img->stripImage();
