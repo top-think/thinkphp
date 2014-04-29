e525e06569eadd49ccd562f6ca042f7406f23092
thinkphp <thinkphp@qq.com>
2014-04-29 11:47:46 +0800

    改进定义TMPL_PATH后的模板路径定义

--- ThinkPHP/Library/Think/View.class.php
+++ ThinkPHP/Library/Think/View.class.php
@@ -155,7 +155,7 @@ class View {
             if(C('VIEW_PATH')){ // 模块设置独立的视图目录
                 $tmplPath   =   C('VIEW_PATH');
             }else{  // 定义TMPL_PATH 改变全局的视图目录到模块之外
-                $tmplPath   =   defined('TMPL_PATH')? TMPL_PATH.$module : APP_PATH.$module.'/'.C('DEFAULT_V_LAYER').'/';
+                $tmplPath   =   defined('TMPL_PATH')? TMPL_PATH.$module.'/' : APP_PATH.$module.'/'.C('DEFAULT_V_LAYER').'/';
             }
             define('THEME_PATH', $tmplPath.$theme);
         }
