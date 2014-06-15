2fd2d7ddcf00f08736671eade6249a6bf71b2264
ThinkPHP <thinkphp@qq.com>
2014-06-15 22:17:29 +0800

    3.2.3beta标记

--- ThinkPHP/ThinkPHP.php
+++ ThinkPHP/ThinkPHP.php
@@ -20,7 +20,7 @@ define('MEMORY_LIMIT_ON',function_exists('memory_get_usage'));
 if(MEMORY_LIMIT_ON) $GLOBALS['_startUseMems'] = memory_get_usage();
 
 // 版本信息
-const THINK_VERSION     =   '3.2.2';
+const THINK_VERSION     =   '3.2.3beta';
 
 // URL 模式定义
 const URL_COMMON        =   0;  //普通模式
