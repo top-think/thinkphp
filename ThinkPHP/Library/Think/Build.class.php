3ef762592bf7593c12ed8dfe95de241e9ea54fcb
joy2fun <web@css.js.cn>
2014-06-25 16:07:37 +0800

    自动创建模型模块的父目录  直接调用\Think\Build::buildController和buildModel的情况下创建目录

--- ThinkPHP/Library/Think/Build.class.php
+++ ThinkPHP/Library/Think/Build.class.php
@@ -122,6 +122,10 @@ class [MODEL]Model extends Model {
             if(!C('APP_USE_NAMESPACE')){
                 $content    =   preg_replace('/namespace\s(.*?);/','',$content,1);
             }
+            $dir = dirname($file);
+            if(!is_dir($dir)){
+                mkdir($dir, 0755, true);
+            }
             file_put_contents($file,$content);
         }
     }
@@ -133,6 +137,10 @@ class [MODEL]Model extends Model {
             $content = str_replace(array('[MODULE]','[MODEL]'),array($module,$model),self::$model);
             if(!C('APP_USE_NAMESPACE')){
                 $content    =   preg_replace('/namespace\s(.*?);/','',$content,1);
+            }
+            $dir = dirname($file);
+            if(!is_dir($dir)){
+                mkdir($dir, 0755, true);
             }
             file_put_contents($file,$content);
         }
