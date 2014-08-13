235eb6dce47e64830169d778f0d0f2347bc7c58d
bowen han <fancycode@gmail.com>
2014-08-13 19:58:09 +0800

    Update Controller.class.php,Fixed Issue #161
    修改了ajaxReturn函数,增加了一个可选参数json_option,不会影响以前的代码
    修复了Issue #161的 ajaxreturn传递json_encode参数的问题,现在可以通过json_option变量向json_encode传递参数,ajaxReturn功能更加强大了

--- ThinkPHP/Library/Think/Controller.class.php
+++ ThinkPHP/Library/Think/Controller.class.php
@@ -204,15 +204,16 @@ abstract class Controller {
      * @access protected
      * @param mixed $data 要返回的数据
      * @param String $type AJAX返回数据格式
+     * @param int $json_option 传递给json_encode的option参数
      * @return void
      */
-    protected function ajaxReturn($data,$type='') {
+    protected function ajaxReturn($data,$type='',$json_option=0) {
         if(empty($type)) $type  =   C('DEFAULT_AJAX_RETURN');
         switch (strtoupper($type)){
             case 'JSON' :
                 // 返回JSON数据格式到客户端 包含状态信息
                 header('Content-Type:application/json; charset=utf-8');
-                exit(json_encode($data));
+                exit(json_encode($data,$json_option));
             case 'XML'  :
                 // 返回xml格式数据
                 header('Content-Type:text/xml; charset=utf-8');
@@ -221,7 +222,7 @@ abstract class Controller {
                 // 返回JSON数据格式到客户端 包含状态信息
                 header('Content-Type:application/json; charset=utf-8');
                 $handler  =   isset($_GET[C('VAR_JSONP_HANDLER')]) ? $_GET[C('VAR_JSONP_HANDLER')] : C('DEFAULT_JSONP_HANDLER');
-                exit($handler.'('.json_encode($data).');');  
+                exit($handler.'('.json_encode($data,$json_option).');');  
             case 'EVAL' :
                 // 返回可执行的js脚本
                 header('Content-Type:text/html; charset=utf-8');
