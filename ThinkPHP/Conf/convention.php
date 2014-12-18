7e442879674561e1ad45dac08fedff51e73efd07
wilson <wilson@localhost>
2014-12-18 20:59:47 +0800

    修正3.1中默认过滤方法的常量

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -137,7 +137,7 @@ return  array(
     'VAR_PATHINFO'          => 's',	// PATHINFO 兼容模式获取变量例如 ?s=/module/action/id/1 后面的参数取决于URL_PATHINFO_DEPR
     'VAR_URL_PARAMS'        => '_URL_', // PATHINFO URL参数变量
     'VAR_TEMPLATE'          => 't',		// 默认模板切换变量
-    'VAR_FILTERS'           =>  'filter_exp',     // 全局系统变量的默认过滤方法 多个用逗号分割
+    'VAR_FILTERS'           =>  'think_filter',     // 全局系统变量的默认过滤方法 多个用逗号分割
 
     'OUTPUT_ENCODE'         =>  false, // 页面压缩输出
     'HTTP_CACHE_CONTROL'    =>  'private', // 网页缓存控制
