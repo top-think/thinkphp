85e7efb3dc7c6e18ceb89ad4d040957885f50ae5
thinkphp <thinkphp@qq.com>
2014-12-21 21:51:13 +0800

    查询表达式同时支持not in/notin 和 not between/notbetween

--- ThinkPHP/Common/functions.php
+++ ThinkPHP/Common/functions.php
@@ -810,7 +810,7 @@ function think_filter(&$value){
     // TODO 其他安全过滤
 
     // 过滤查询特殊字符
-    if(preg_match('/^(EXP|NEQ|GT|EGT|LT|ELT|OR|LIKE|NOTLIKE|NOTBETWEEN|BETWEEN|NOTIN|IN)$/i',$value)){
+    if(preg_match('/^(EXP|NEQ|GT|EGT|LT|ELT|OR|LIKE|NOTLIKE|NOTBETWEEN|NOT BETWEEN|BETWEEN|NOTIN|NOT IN|IN)$/i',$value)){
         $value .= ' ';
     }
 }
