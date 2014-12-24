8acd408cb12e597156171c5b82d00469ada121c8
thinkphp <thinkphp@qq.com>
2014-12-24 21:17:08 +0800

    增加DATA_CACHE_KEY参数 用于文件方式的缓存文件名规则哈希

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -60,6 +60,7 @@ return  array(
     'DB_PWD'                =>  '',          // 密码
     'DB_PORT'               =>  '',        // 端口
     'DB_PREFIX'             =>  '',    // 数据库表前缀
+    'DB_PARAMS'          	=>  array(), // 数据库连接参数    
     'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
     'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
@@ -75,6 +76,7 @@ return  array(
     'DATA_CACHE_PREFIX'     =>  '',     // 缓存前缀
     'DATA_CACHE_TYPE'       =>  'File',  // 数据缓存类型,支持:File|Db|Apc|Memcache|Shmop|Sqlite|Xcache|Apachenote|Eaccelerator
     'DATA_CACHE_PATH'       =>  TEMP_PATH,// 缓存路径设置 (仅对File方式缓存有效)
+    'DATA_CACHE_KEY'        =>  '',	// 缓存文件KEY (仅对File方式缓存有效)    
     'DATA_CACHE_SUBDIR'     =>  false,    // 使用子目录缓存 (自动根据缓存标识的哈希创建子目录)
     'DATA_PATH_LEVEL'       =>  1,        // 子目录缓存级别
 
