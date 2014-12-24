b4b5954af08b54e2a611603532c4c18ca14fb71a
thinkphp <thinkphp@qq.com>
2014-12-24 15:58:53 +0800

    删除一些无用的配置参数

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -60,18 +60,13 @@ return  array(
     'DB_PWD'                =>  '',          // 密码
     'DB_PORT'               =>  '',        // 端口
     'DB_PREFIX'             =>  '',    // 数据库表前缀
-    'DB_FIELDTYPE_CHECK'    =>  false,       // 是否进行字段类型检查
+    'DB_DEBUG'  			=>  TRUE, // 数据库调试模式 开启后可以记录SQL日志
     'DB_FIELDS_CACHE'       =>  true,        // 启用字段缓存
     'DB_CHARSET'            =>  'utf8',      // 数据库编码默认采用utf8
     'DB_DEPLOY_TYPE'        =>  0, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
     'DB_RW_SEPARATE'        =>  false,       // 数据库读写是否分离 主从式有效
     'DB_MASTER_NUM'         =>  1, // 读写分离后 主服务器数量
     'DB_SLAVE_NO'           =>  '', // 指定从服务器序号
-    'DB_SQL_BUILD_CACHE'    =>  false, // 数据库查询的SQL创建缓存
-    'DB_SQL_BUILD_QUEUE'    =>  'file',   // SQL缓存队列的缓存方式 支持 file xcache和apc
-    'DB_SQL_BUILD_LENGTH'   =>  20, // SQL缓存的队列长度
-    'DB_SQL_LOG'            =>  false, // SQL执行日志记录
-    'DB_BIND_PARAM'         =>  false, // 数据库写入数据自动参数绑定
 
     /* 数据缓存设置 */
     'DATA_CACHE_TIME'       =>  0,      // 数据缓存有效期 0表示永久缓存
