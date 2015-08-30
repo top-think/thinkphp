fc8fd22860a736d8a2019298e1e1b53d355e1936
772807886 <jinliming2@qq.com>
2015-08-30 23:01:06 +0800

    Update functions.php

--- ThinkPHP/Common/functions.php
+++ ThinkPHP/Common/functions.php
@@ -1447,12 +1447,13 @@ function load_ext_file($path) {
  * 获取客户端IP地址
  * @param integer $type 返回类型 0 返回IP地址 1 返回IPV4地址数字
  * @param boolean $adv 是否进行高级模式获取（有可能被伪装） 
+ * @param boolean $renew 强制重新获取
  * @return mixed
  */
-function get_client_ip($type = 0,$adv=false) {
+function get_client_ip($type = 0,$adv=false,$renew=false) {
     $type       =  $type ? 1 : 0;
     static $ip  =   NULL;
-    if ($ip !== NULL) return $ip[$type];
+    if ($renew && $ip !== NULL) return $ip[$type];
     if($adv){
         if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             $arr    =   explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
